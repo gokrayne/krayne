6fcf5bcd838fca6515be5218e57293793016fdb9
MyNameYui <zerotwox3.cheat@gmail.com>
2023-11-02 08:16:06 +0700

    Update Cache

--- main.go
+++ main.go
@@ -1,13 +1,28 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
+)
 
 func main() {
-    app := fiber.New()
+	app := fiber.New()
+	// Initialize default config
+	app.Use(cache.New())
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World 👋!")
-    })
+	// Or extend your config for customization
+	app.Use(cache.New(cache.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.Query("noCache") == "true"
+		},
+		Expiration:   30 * time.Minute,
+		CacheControl: true,
+	}))
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
 
-    app.Listen(":3000")
+	app.Listen(":3000")
 }

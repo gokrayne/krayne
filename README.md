482b29e69046bf1613c15784d8f189f8c424e157
MyNameYui <zerotwo.killer1993@gmail.com>
2023-11-03 17:54:28 +0700

    :zap: fast http

---
+++ README.md
@@ -0,0 +1,2 @@
+# krayne
+# Hello World
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cache"
+)
+
+func main() {
+	app := fiber.New()
+	// Initialize default config
+	app.Use(cache.New())
+
+	// Or extend your config for customization
+	app.Use(cache.New(cache.Config{
+		Next: func(c *fiber.Ctx) bool {
+			return c.Query("noCache") == "true"
+		},
+		Expiration:   30 * time.Minute,
+		CacheControl: true,
+	}))
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+	app.Get("/a", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
+	app.Listen(":3000")
+}
